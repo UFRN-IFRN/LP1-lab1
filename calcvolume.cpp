c74299a686e82091c5748f503072189d95127b81
Bianca Santiago <bianca.santiago72@gmail.com>
2017-03-12 18:03:42 -0300

    Update calcvolume.cpp
    Última versão do calcvolume.cpp antes da documentação

--- calcvolume.cpp
+++ calcvolume.cpp
@@ -4,34 +4,18 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-float altura, raio,  area_base, aresta, aresta1, aresta2, aresta3; 
-
-void informarMedidasVolumePiramide(){
-	cout << "Digite a área da base da pirâmide: " << endl;
-    cin >> area_base;
-    cout << "Digite a altura da pirâmide: " << endl;
-    cin >> altura;
+void mostrarVolumePiramide(float area_base, float altura){
     cout << "Volume da pirâmide: " << calcularVolumePiramide(area_base, altura) << endl;
 }
 
-void informarMedidasVolumeCubo(){
-    cout << "Digite o tamanho de uma aresta do cubo: " << endl;
-    cin >> aresta;
+void mostrarVolumeCubo(float aresta){
     cout << "Volume do cubo: " << calcularVolumeCubo(aresta) << endl;
 }
 
-void informarMedidasVolumeParalelepipedo(){
-    cout << "Digite o tamanho da 1ª aresta do paralelepípedo: " << endl;
-    cin >> aresta1;
-    cout << "Digite o tamanho da 2ª aresta do paralelepípedo: " << endl;
-    cin >> aresta2;
-    cout << "Digite o tamanho da 3ª aresta do paralelepípedo: " << endl;
-    cin >> aresta3;
+void mostrarVolumeParalelepipedo(float aresta1, float aresta2, float aresta3){
     cout << "Volume do paralelepípedo: " << calcularVolumeParalelepipedo(aresta1, aresta2, aresta3) << endl;
 }
 
-void informarMedidasVolumeEsfera(){
-	cout << "Digite o tamanho do raio da esfera: " << endl;
-    cin >> raio;
+void mostrarVolumeEsfera(float raio){
     cout << "Volume da esfera: " << calcularVolumeEsfera(raio) << endl;
 }
