a75b8a52551986f22f04690eb3a12de38d6f9a83
Bianca Santiago <bianca.santiago72@gmail.com>
2017-03-12 17:59:31 -0300

    Última versão do calcarea.h antes da documentação

--- calcarea.h
+++ calcarea.h
@@ -3,13 +3,13 @@
 
 #include "area.h"
 
-void informarMedidasAreaTriangulo();
-void informarMedidasAreaRetangulo();
-void informarMedidasAreaQuadrado();
-void informarMedidasAreaCirculo();
-void informarMedidasAreaPiramide();
-void informarMedidasAreaCubo();
-void informarMedidasAreaParalelepipedo();
-void informarMedidasAreaEsfera();
+void mostrarAreaTriangulo(float altura, float base);
+void mostrarAreaRetangulo(float base, float altura);
+void mostrarAreaQuadrado(float lado);
+void mostrarAreaCirculo(float raio);
+void mostrarAreaPiramide(float area_base, float area_lateral);
+void mostrarAreaCubo(float aresta);
+void mostrarAreaParalelepipedo(float aresta1, float aresta2, float aresta3);
+void mostrarAreaEsfera(float raio);
 
 #endif
