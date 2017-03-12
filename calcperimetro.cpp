dca12468e6ecacc7b83fb5164a61bbb97c0219be
Bianca Santiago <bianca.santiago72@gmail.com>
2017-03-12 18:00:53 -0300

    Update calcperimetro.cpp
    Última versão do calcperimetro.cpp antes da documentação

--- calcperimetro.cpp
+++ calcperimetro.cpp
@@ -4,34 +4,18 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-float base, altura, raio, lado, lado2, lado3; 
-
-void informarMedidasPerimetroTriangulo(){
-    cout << "Digite o tamanho da base (1º lado) do triângulo equilátero: " << endl;
-    cin >> base;
-    cout << "Digite o tamanho do 2º lado do triângulo equilátero: " << endl;
-    cin >> lado2;
-    cout << "Digite o tamanho do 3º lado do triângulo equilátero: " << endl;
-    cin >> lado3;
+void mostrarPerimetroTriangulo(float base, float lado2, float lado3){
     cout << "Perímetro do triângulo: " << calcularPerimetroTriangulo(base, lado2, lado3) << endl;
 }
 
-void informarMedidasPerimetroRetangulo(){
-    cout << "Digite o tamanho da base do retângulo: " << endl;
-    cin >> base;
-    cout << "Digite o tamanho da altura do retângulo: " << endl;
-    cin >> altura;
+void mostrarPerimetroRetangulo(float base, float altura){
     cout << "Perímetro do retângulo: " << calcularPerimetroRetangulo(base, altura) << endl;
 }
 
-void informarMedidasPerimetroQuadrado(){
-    cout << "Digite o tamanho de um lado do quadrado: " << endl;
-    cin >> lado;   
+void mostrarPerimetroQuadrado(float lado){ 
     cout << "Perímetro do quadrado: " << calcularPerimetroQuadrado(lado) << endl;
 }
 
-void informarMedidasPerimetroCirculo(){
-    cout << "Digite o tamanho do raio do círculo: " << endl;
-    cin >> raio;
+void mostrarPerimetroCirculo(float raio){
     cout << "Perímetro do círculo: " << calcularPerimetroCirculo(raio) << endl;
 }
